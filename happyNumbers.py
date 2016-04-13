295c195fff804aa117b0dd59dea7b22ae9530591
joemcilheran <joe.mcilheran@gmail.com>
2016-04-13 00:31:49 -0500

    got it working

--- happyNumbers.py
+++ happyNumbers.py
@@ -1,12 +1,21 @@
-def summm(begin):
-    posint = begin + 1
-    posint = list(posint)
-    squarelist = (int(digit)**2 for digit in posint)
-    listsum = sum(squarelist)  
-    if (int(listsum) == 1):
-        print(posint)
-    elif (int(listsum) == 4):
+def allNumbers():
+    happyNumbers = []
+    for posint in range(1,10000):
+        happy = happycheck(posint)
+        if happy == True and len(happyNumbers) < 21:
+            happyNumbers.append(posint)
+        elif len(happyNumbers) >= 20:
+            break
+    print(happyNumbers)
+    
+def happycheck(number):
+    seenNumbers = set()
+    while number != 1:
+        number = sum(int(digit)**2 for digit in str(number))
+        if number in seenNumbers:
+            return False
+        seenNumbers.add(number)
+    return True
+            
         
-        
-        
-summm()
+allNumbers()
